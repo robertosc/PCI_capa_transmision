c761dbe011fa30e3e7d6b3bdf723b50a8f1814c0
robertosc <roberto.sanchezcardenas@ucr.ac.cr>
2020-11-03 19:44:23 -0600

    arbitro problemas prioridad

--- arbitro_muxes/arbitro_mux.v
+++ arbitro_muxes/arbitro_mux.v
@@ -1,20 +1,23 @@
 module arbitro_mux(input reset_L, clk,
-                    input [5:0] VC0,
-                    input [5:0] VC1,
-                    input pop_delay_VCO,
-                    output reg [5:0] mux_arbitro_1);
+					input [5:0] VC0,
+					input [5:0] VC1,
+					input pop_delay_VCO, pop_delay_VC1,
+					input VC0_empty,
+					output reg [5:0] mux_arbitro_1);
 
-    always@(posedge clk) begin
-       if(~reset_L) begin
-           mux_arbitro_1 <= 0;
-       end
-       else begin
-            case (pop_delay_VCO)
-                0: mux_arbitro_1 <= VC1;
-                1: mux_arbitro_1 <= VC0;
-            endcase
-       end 
-    end
+	always@(posedge clk) begin
+		if(~reset_L) begin
+			mux_arbitro_1 <= 0;
+		end
+		else begin
+			if (~VC0_empty) begin
+				mux_arbitro_1 <= VC0;
+			end
+			else begin
+				if()
+			end
+		end 
+	end
 
 
 endmodule
