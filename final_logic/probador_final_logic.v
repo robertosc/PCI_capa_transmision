4b03649434328e625ffcb0abf0f1185f6f863b31
robertosc <roberto.sanchezcardenas@ucr.ac.cr>
2020-11-09 17:16:30 -0600

    final logic parece bien

--- final_logic/probador_final_logic.v
+++ final_logic/probador_final_logic.v
@@ -13,13 +13,14 @@ module probador_final_logic(output reg [5:0] data_out_VC0, data_out_VC1,
     empty_fifo_VC0 <= 0;
     empty_fifo_VC1 <= 0;
     D0_pop <= 0;
-    D1_pop <= 1;
+    D1_pop <= 0;
 
 	@(posedge clk);
 	@(posedge clk);
     reset_L <= 1;
     data_out_VC0 <= 6'b110100;
     data_out_VC1 <= 6'b110110;
+    D1_pop = 1;
     @(posedge clk);
 
     data_out_VC0 <= 6'b100101;
